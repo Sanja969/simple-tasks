8e85e55a287fba46d9d3952b617a5685e21b395d
Sanja <sanja1mandic@gmail.com>
2024-01-07 18:23:03 +0100

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require("morgan")
+
+const app = express();
+app.use(cors());
+app.use(morgan("tiny")) 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = app;
