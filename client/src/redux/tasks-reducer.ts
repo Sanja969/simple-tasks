d562cbc0e6ef787a02947fb9f5d3353431c3e435
Sanja <sanja1mandic@gmail.com>
2024-01-08 12:27:41 +0100

    Create redux actions for getting, deleting, updating and deleting tasks

--- client/src/redux/tasks-reducer.ts
+++ client/src/redux/tasks-reducer.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux';
 
 const url = 'http://localhost:8000/api/v1/tasks';
 const GET_TASKS = 'GET_TASKS';
+const GET_TASK = 'GET_TASK';
 
 const initialState = {tasks: [], task: {}};
 
@@ -22,6 +23,8 @@ const tasksReducer = (state = initialState, action: GetTasksAction) => {
   switch (action.type) {
     case GET_TASKS:
       return {...state, tasks: [...action.payload as Task[]]};
+    case GET_TASK:
+      return {...state, task: action.payload as Task};
     default:
       return state;
   }
@@ -38,6 +41,97 @@ export const getTasks = () => async (dispatch: Dispatch) => {
       type: GET_TASKS,
       payload: tasks,
     });
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+};
+
+export const getTask = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    const response = await fetch(`${url}/${id}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const task = await response.json();
+  
+    dispatch({
+      type: GET_TASK,
+      payload: task,
+    });
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+
+};
+
+export const saveTask = (task: Task) => async(dispatch: Dispatch) => {
+  try {
+    const response = await fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {
+         "Content-Type": "application/json"
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const json = await response.json();
+  
+    dispatch({
+      type: GET_TASKS,
+      payload: json,
+    })
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+};
+
+export const updateTask = (task: Task) => async(dispatch: Dispatch) => {
+  const { id, ...other } = task;
+
+  try {
+    const response = await fetch(`${url}/${id}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(other),
+      headers: {
+       "Content-Type": "application/json"
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const json = await response.json();
+
+    dispatch({
+      type: GET_TASKS,
+      payload: json,
+    })
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+};
+
+export const deleteTask = (id: string) => async(dispatch: Dispatch) => {
+  try {
+    await fetch(`${url}/${id}`,
+    {
+      method: 'DELETE',
+      headers: {
+       "Content-Type": "application/json"
+      },
+    });
+
+    dispatch({
+      type: GET_TASKS,
+      payload: id,
+    })
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
   }
